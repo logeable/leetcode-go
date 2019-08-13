306c446e2110fac136fc7745efdbec4dd31189d9
logeable <logeable@gmail.com>
2019-08-13 13:01:44 +0800

    two sum

---
+++ solution/two-sum/two-sum.go
@@ -0,0 +1,23 @@
+package solution
+
+func twoSum1(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if target == nums[i]+nums[j] {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
+
+func twoSum2(nums []int, target int) []int {
+	cache := make(map[int]int, len(nums))
+	for i, x := range nums {
+		if v, ok := cache[target-x]; ok {
+			return []int{v, i}
+		}
+		cache[x] = i
+	}
+	return nil
+}
---
+++ solution/two-sum/two-sum_test.go
@@ -0,0 +1,69 @@
+package solution
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type param struct {
+	nums   []int
+	target int
+}
+
+var (
+	table = []struct {
+		param  param
+		answer []int
+	}{
+		{
+			param{
+				[]int{2, 7, 11, 15},
+				9,
+			},
+			[]int{0, 1},
+		},
+		{
+			param{
+				[]int{1, 5, 9, 20},
+				29,
+			},
+			[]int{2, 3},
+		},
+		{
+			param{
+				[]int{1, 5, 9, 20},
+				2,
+			},
+			nil,
+		},
+	}
+)
+
+func TestTwoSum1(t *testing.T) {
+
+	for _, row := range table {
+		ans := twoSum1(row.param.nums, row.param.target)
+		assert.Equal(t, ans, row.answer)
+	}
+}
+
+func BenchmarkTwoSum1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		twoSum1(table[0].param.nums, table[0].param.target)
+	}
+}
+
+func TestTwoSum2(t *testing.T) {
+
+	for _, row := range table {
+		ans := twoSum2(row.param.nums, row.param.target)
+		assert.Equal(t, ans, row.answer)
+	}
+}
+
+func BenchmarkTwoSum2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		twoSum2(table[0].param.nums, table[0].param.target)
+	}
+}

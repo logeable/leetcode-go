dfa4efb02ef89ec3baa41f1adc665769092b9b89
logeable <logeable@gmail.com>
2022-02-08 22:19:42 +0800

    solution for 剑指 Offer 58 - II. 左旋转字符串

--- solutions/zuo-xuan-zhuan-zi-fu-chuan-lcof/solution.go
+++ solutions/zuo-xuan-zhuan-zi-fu-chuan-lcof/solution.go
@@ -1,15 +1,5 @@
 package solution
 
 func reverseLeftWords(s string, n int) string {
-	rs := []rune(s)
-	reverse(rs[:n])
-	reverse(rs[n:])
-	reverse(rs)
-	return string(rs)
-}
-
-func reverse(rs []rune) {
-	for i, j := 0, len(rs)-1; i < j; i, j = i+1, j-1 {
-		rs[i], rs[j] = rs[j], rs[i]
-	}
+	return s[n:] + s[:n]
 }

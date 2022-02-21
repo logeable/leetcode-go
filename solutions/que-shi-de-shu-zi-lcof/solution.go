286b00011cad3c45eb81b257f3dc28dae08ea56e
logeable <logeable@gmail.com>
2022-02-21 23:22:44 +0800

    solution for 剑指 Offer 53 (二分法)

--- solutions/que-shi-de-shu-zi-lcof/solution.go
+++ solutions/que-shi-de-shu-zi-lcof/solution.go
@@ -1,10 +1,14 @@
 package solution
 
 func missingNumber(nums []int) int {
-	for i, n := range nums {
-		if i != n {
-			return i
+	l, r := 0, len(nums)-1
+	for l <= r {
+		m := l + ((r - l) >> 1)
+		if nums[m] == m {
+			l = m + 1
+		} else {
+			r = m - 1
 		}
 	}
-	return len(nums)
+	return l
 }

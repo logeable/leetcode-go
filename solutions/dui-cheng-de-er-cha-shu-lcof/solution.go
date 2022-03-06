cbfc7d2a37bd80a6b6010477ac339d09b0d26a73
logeable <logeable@gmail.com>
2022-03-06 10:35:39 +0800

    solution for 剑指 Offer 28. 对称的二叉树

--- solutions/dui-cheng-de-er-cha-shu-lcof/solution.go
+++ solutions/dui-cheng-de-er-cha-shu-lcof/solution.go
@@ -14,34 +14,16 @@ func isSymmetric(root *types.TreeNode) bool {
 	if root == nil {
 		return true
 	}
-	q := []*types.TreeNode{root}
 
-	for len(q) != 0 {
-		var layer []*types.TreeNode
-		length := len(q)
-		for i := 0; i < length; i++ {
-			node := q[0]
-			q = q[1:]
+	return sym(root.Left, root.Right)
+}
 
-			layer = append(layer, node)
-			if node != nil {
-				q = append(q, node.Left)
-				q = append(q, node.Right)
-			}
-		}
-		l, r := 0, length-1
-		for l < r {
-			if layer[l] == nil && layer[r] == nil {
-				l++
-				r--
-			} else if layer[l] != nil && layer[r] != nil && layer[l].Val == layer[r].Val {
-				l++
-				r--
-			} else {
-				return false
-			}
-		}
-
+func sym(a *types.TreeNode, b *types.TreeNode) bool {
+	if a == nil && b == nil {
+		return true
+	} else if a != nil && b != nil {
+		return a.Val == b.Val && sym(a.Left, b.Right) && sym(a.Right, b.Left)
+	} else {
+		return false
 	}
-	return true
 }

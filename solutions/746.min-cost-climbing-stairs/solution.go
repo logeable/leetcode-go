cce3aa57ee2706025950a4088cdcec1cdca7246c
logeable <logeable@gmail.com>
2022-03-10 23:27:14 +0800

    solution for 746. 使用最小花费爬楼梯

---
+++ solutions/746.min-cost-climbing-stairs/solution.go
@@ -0,0 +1,24 @@
+package solution
+
+func minCostClimbingStairs(cost []int) int {
+	if len(cost) == 0 {
+		return 0
+	}
+	if len(cost) == 1 {
+		return 0
+	}
+	dp := make([]int, len(cost)+1)
+	dp[0] = 0
+	dp[0] = 0
+	for i := 2; i <= len(cost); i++ {
+		dp[i] = min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2])
+	}
+	return dp[len(cost)]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}

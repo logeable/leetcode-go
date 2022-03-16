f5c2186b7f2fcaf750e3c2bd8010364caa402657
logeable <logeable@gmail.com>
2022-03-16 23:43:13 +0800

    solution for 121. 买卖股票的最佳时机

---
+++ solutions/121.best-time-to-buy-and-sell-stock/solution.go
@@ -0,0 +1,16 @@
+package solution
+
+func maxProfit(prices []int) int {
+	minPrice := prices[0]
+	profit := 0
+	for i := 1; i < len(prices); i++ {
+		if prices[i] < minPrice {
+			minPrice = prices[i]
+		}
+
+		if prices[i]-minPrice > profit {
+			profit = prices[i] - minPrice
+		}
+	}
+	return profit
+}

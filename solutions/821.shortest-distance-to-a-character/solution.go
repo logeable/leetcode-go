b22ccfedf6604d1f4209ff1db6701aa92ab1bd9e
logeable <logeable@gmail.com>
2022-03-19 00:04:44 +0800

    solution for 821. 字符的最短距离

--- solutions/821.shortest-distance-to-a-character/solution.go
+++ solutions/821.shortest-distance-to-a-character/solution.go
@@ -1,13 +1,30 @@
 package solution
 
+import "math"
+
 func shortestToChar(s string, c byte) []int {
 	ret := make([]int, len(s))
+	prev := -math.MaxInt / 2
+	for i := 0; i < len(ret); i++ {
+		if s[i] == c {
+			prev = i
+		}
+		ret[i] = i - prev
+	}
+	prev = math.MaxInt
+
+	for i := len(ret) - 1; i >= 0; i-- {
+		if s[i] == c {
+			prev = i
+		}
+		ret[i] = min(ret[i], prev-i)
+	}
 	return ret
 }
 
-func abs(a, b int) int {
-	if a > b {
-		return a - b
+func min(a, b int) int {
+	if a < b {
+		return a
 	}
-	return b - a
+	return b
 }

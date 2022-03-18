ac48015846cfa32e0b39156af5b8330872e4ab1a
logeable <logeable@gmail.com>
2022-03-18 23:56:54 +0800

    821. 字符的最短距离

---
+++ solutions/821.shortest-distance-to-a-character/solution.go
@@ -0,0 +1,13 @@
+package solution
+
+func shortestToChar(s string, c byte) []int {
+	ret := make([]int, len(s))
+	return ret
+}
+
+func abs(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}

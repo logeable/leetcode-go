0a9934b74c9356a99bdd9ac178fd900da96d09c6
logeable <logeable@gmail.com>
2022-03-28 23:21:29 +0800

    solution for 693. 交替位二进制数

--- solutions/693.binary-number-with-alternating-bits/solution.go
+++ solutions/693.binary-number-with-alternating-bits/solution.go
@@ -1,15 +1,6 @@
 package solution
 
 func hasAlternatingBits(n int) bool {
-	prev := n & 1
-	n >>= 1
-	for n > 0 {
-		b := n & 1
-		if b == prev {
-			return false
-		}
-		prev = b
-		n >>= 1
-	}
-	return true
+	a := n ^ n>>1
+	return a&(a+1) == 0
 }

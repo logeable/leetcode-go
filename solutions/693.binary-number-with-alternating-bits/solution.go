4c0fef83ef625efb97f7aae1d1acae4af636037f
logeable <logeable@gmail.com>
2022-03-28 23:18:46 +0800

    solution for 693. 交替位二进制数

--- solutions/693.binary-number-with-alternating-bits/solution.go
+++ solutions/693.binary-number-with-alternating-bits/solution.go
@@ -1,24 +1,15 @@
 package solution
 
 func hasAlternatingBits(n int) bool {
-	bits := tobits(n)
-	if len(bits) == 1 {
-		return true
-	}
-
-	for i := 1; i < len(bits); i++ {
-		if bits[i] == bits[i-1] {
+	prev := n & 1
+	n >>= 1
+	for n > 0 {
+		b := n & 1
+		if b == prev {
 			return false
 		}
-	}
-	return true
-}
-
-func tobits(n int) []byte {
-	var ret []byte
-	for n > 0 {
-		ret = append(ret, byte(n&1))
+		prev = b
 		n >>= 1
 	}
-	return ret
+	return true
 }

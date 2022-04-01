c17ca21ca352f2e9952d078f6645e4e14f01134b
logeable <logeable@gmail.com>
2022-04-02 00:03:45 +0800

    solution for 221. 最大正方形

--- solutions/221.maximal-square/solution.go
+++ solutions/221.maximal-square/solution.go
@@ -3,9 +3,9 @@ package solution
 func maximalSquare(matrix [][]byte) int {
 	dp := make([][]int, len(matrix))
 	max := 0
-	for i := 0; i < len(matrix); i++ {
+	for i := 0; i < len(dp); i++ {
 		dp[i] = make([]int, len(matrix[i]))
-		for j := 0; j < len(matrix[i]); i++ {
+		for j := 0; j < len(dp[i]); j++ {
 			if matrix[i][j] == '1' {
 				dp[i][j] = 1
 				max = 1
@@ -14,13 +14,15 @@ func maximalSquare(matrix [][]byte) int {
 	}
 	for i := 1; i < len(matrix); i++ {
 		for j := 1; j < len(matrix[i]); j++ {
-			dp[i][j] = min(min(dp[i-1][j-1], dp[i][j-1]), dp[i-1][j]) + 1
-			if dp[i][j] > max {
-				max = dp[i][j]
+			if matrix[i][j] == '1' {
+				dp[i][j] = min(min(dp[i-1][j-1], dp[i][j-1]), dp[i-1][j]) + 1
+				if dp[i][j] > max {
+					max = dp[i][j]
+				}
 			}
 		}
 	}
-	return max
+	return max * max
 }
 
 func min(a, b int) int {

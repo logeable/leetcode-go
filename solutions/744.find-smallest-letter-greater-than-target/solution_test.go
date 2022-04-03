a37638ef641a1fb54396de38740ab3224a61dbff
logeable <logeable@gmail.com>
2022-04-03 23:42:25 +0800

    solution for 744. 寻找比目标字母大的最小字母

---
+++ solutions/744.find-smallest-letter-greater-than-target/solution_test.go
@@ -0,0 +1,47 @@
+package solution
+
+import "testing"
+
+func Test_nextGreatestLetter(t *testing.T) {
+	type args struct {
+		letters []byte
+		target  byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want byte
+	}{
+		{
+			name: "example 1",
+			args: args{
+				letters: []byte{'c', 'f', 'j'},
+				target:  'a',
+			},
+			want: 'c',
+		},
+		{
+			name: "example 2",
+			args: args{
+				letters: []byte{'c', 'f', 'j'},
+				target:  'c',
+			},
+			want: 'f',
+		},
+		{
+			name: "example 3",
+			args: args{
+				letters: []byte{'c', 'f', 'j'},
+				target:  'd',
+			},
+			want: 'f',
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := nextGreatestLetter(tt.args.letters, tt.args.target); got != tt.want {
+				t.Errorf("nextGreatestLetter() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

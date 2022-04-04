84ae1efe27ee1d5f481b63684299eaf7d91755d1
logeable <logeable@gmail.com>
2022-04-04 23:57:58 +0800

    solution for 307. 区域和检索 - 数组可修改

--- solutions/307.range-sum-query-mutable/solution.go
+++ solutions/307.range-sum-query-mutable/solution.go
@@ -1,18 +1,24 @@
 package solution
 
 type NumArray struct {
+	arr []int
 }
 
 func Constructor(nums []int) NumArray {
-	return NumArray{}
+	return NumArray{arr: nums}
 }
 
 func (this *NumArray) Update(index int, val int) {
+	this.arr[index] = val
 
 }
 
 func (this *NumArray) SumRange(left int, right int) int {
-	return 0
+	ret := 0
+	for i := left; i <= right; i++ {
+		ret += this.arr[i]
+	}
+	return ret
 }
 
 /**

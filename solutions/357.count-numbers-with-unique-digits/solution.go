b38757b6113b0017467f6053f2ba300dd81c8779
logeable <logeable@gmail.com>
2022-04-12 00:01:54 +0800

    solution for 357. 统计各位数字都不同的数字个数

--- solutions/357.count-numbers-with-unique-digits/solution.go
+++ solutions/357.count-numbers-with-unique-digits/solution.go
@@ -1,5 +1,31 @@
 package solution
 
 func countNumbersWithUniqueDigits(n int) int {
-	return 0
+	max := 1
+	for i := 0; i < n; i++ {
+		max *= 10
+	}
+	ans := 0
+	if max > 0 {
+		ans++
+	}
+	for i := 1; i < max; i++ {
+		cache := make(map[int]struct{})
+		n := i
+		found := false
+		for n > 0 {
+			m := n % 10
+			n /= 10
+			if _, ok := cache[m]; ok {
+				found = true
+				break
+			}
+			cache[m] = struct{}{}
+		}
+		if !found {
+			ans++
+		}
+	}
+
+	return ans
 }

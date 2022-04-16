2e9a9131b026219a59e03b9a601a7468f208fdd4
logeable <logeable@gmail.com>
2022-04-17 00:03:05 +0800

    solution for 3. 无重复字符的最长子串

--- solutions/3.longest-substring-without-repeating-characters/solution.go
+++ solutions/3.longest-substring-without-repeating-characters/solution.go
@@ -1,5 +1,23 @@
 package solution
 
 func lengthOfLongestSubstring(s string) int {
-	return 0
+	if len(s) == 0 {
+		return 0
+	}
+	index := make(map[byte]bool)
+	ans := 1
+	end := -1
+	for i := 0; i < len(s); i++ {
+		if i != 0 {
+			delete(index, s[i-1])
+		}
+		for end+1 < len(s) && !index[s[end+1]] {
+			end++
+			index[s[end]] = true
+		}
+		if v := end - i + 1; v > ans {
+			ans = v
+		}
+	}
+	return ans
 }

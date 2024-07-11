2b4b1a90e88d80fe820846802b8ba0ab0a480246
logeable <logeable@gmail.com>
2024-07-11 17:06:55 +0800

    add 1143.longest-common-subsequence

--- solutions/1143.longest-common-subsequence/solution.go
+++ solutions/1143.longest-common-subsequence/solution.go
@@ -1,5 +1,20 @@
 package solution
 
 func longestCommonSubsequence(text1 string, text2 string) int {
-	return 0
+	dp := make([][]int, len(text1)+1)
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, len(text2)+1)
+	}
+
+	for i := 1; i < len(dp); i++ {
+		for j := 1; j < len(dp[0]); j++ {
+			if text1[i-1] == text2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+			}
+		}
+	}
+
+	return dp[len(text1)][len(text2)]
 }
